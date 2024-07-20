fe7884cdea9d9b71b58245934fe5abd432eaa17f
Manohar Udutha <manoharudutha@gmail.com>
2024-07-21 01:15:50 +0530

    Added project info

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-dark text-light p-4">
-        <h6 className="text-center text-light">Made by Sridhar. B</h6>
+        <h6 className="text-center text-light">Made by Manohar Udutha</h6>
     </div>
     )
 };
